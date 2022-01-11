6894052b3475fd59bfdd49b5d282313ac9f3904c
kouya17 <17383696+kouya17@users.noreply.github.com>
2022-01-11 22:18:06 +0900

    feat: add sort method

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Tags from '../components/tags'
 import Posts from '../components/posts'
 import { useState } from "react"
 import Search from '../components/search'
+import Dropdown from '../components/selectBox'
 
 interface TagToPostIds {
   [index: string]: string[];
@@ -15,13 +16,27 @@ interface IdToPost {
   [index: string]: PostData;
 }
 
-function searchPostIds(keyword: string, posts: PostData[]): string[] {
+function isContainKeyword(keyword: string, post: PostData): boolean {
+  return (
+    post.fileData.matter.title.toLowerCase().includes(keyword.toLowerCase())
+    || post.fileData.matter.abstract.toLowerCase().includes(keyword.toLowerCase())
+    || post.fileData.matter.tags.includes(keyword)
+  )
+}
+
+function isContainKeywords(keywords: string[], post: PostData): boolean {
+  const reducer = (pre: number, current: string) => {
+    if (isContainKeyword(current, post)) {
+      return pre + 1
+    }
+    return pre
+  }
+  return keywords.reduce(reducer, 0) === keywords.length
+}
+
+function searchPostIds(keywords: string[], posts: PostData[]): string[] {
   const reducer = (pre: string[], current: PostData) => {
-    if (
-      current.fileData.matter.title.toLowerCase().includes(keyword.toLowerCase())
-      || current.fileData.matter.abstract.toLowerCase().includes(keyword.toLowerCase())
-      || current.fileData.matter.tags.includes(keyword)
-    ) {
+    if (isContainKeywords(keywords, current)) {
       pre.push(current.fileData.id)
     }
     return pre
@@ -39,7 +54,7 @@ function getPostIdsByOR(keywords: string[], tagToPostIds: TagToPostIds): string[
   return keywords.reduce(reducer, [])
 }
 
-function getPostIdsByAND(tags: string[], text: string, tagToPostIds: TagToPostIds, allPostIds: string[], idToPost: IdToPost): string[] {
+function getPostIdsByAND(tags: string[], texts: string[], tagToPostIds: TagToPostIds, allPostIds: string[], idToPost: IdToPost): string[] {
   const reducer = (pre: string[], current: string) => {
     if (tagToPostIds[current]) {
       return pre.filter(item => tagToPostIds[current].includes(item))
@@ -48,15 +63,15 @@ function getPostIdsByAND(tags: string[], text: string, tagToPostIds: TagToPostId
   }
   const tagMatchIds = tags.reduce(reducer, allPostIds)
   // if text is empty, skip to search by text
-  if (text === '') {
+  if (texts === []) {
     return tagMatchIds
   }
   const tagMatchPosts = tagMatchIds.map(id => idToPost[id])
-  return searchPostIds(text, tagMatchPosts)
+  return searchPostIds(texts, tagMatchPosts)
 }
 
-function getPostsByTagsAndText(tags: string[], text: string, tagToPostIds: TagToPostIds, idToPost: IdToPost, allPostIds: string[]): PostData[] {
-  const ids = getPostIdsByAND(tags, text, tagToPostIds, allPostIds, idToPost)
+function getPostsByTagsAndText(tags: string[], texts: string[], tagToPostIds: TagToPostIds, idToPost: IdToPost, allPostIds: string[]): PostData[] {
+  const ids = getPostIdsByAND(tags, texts, tagToPostIds, allPostIds, idToPost)
   const idToPostsReducer = (pre: PostData[], current: string) => {
     if (idToPost[current]) {
       pre.push(idToPost[current])
@@ -76,11 +91,12 @@ export default function Home({
   const [renderPosts, setRenderPosts] = useState(allPostsData)
   const updateRenderPosts = (tags: string[], searchText: string) => {
     // if tags have searchText, set search Text empty to reduce calculation
-    if (tags.includes(searchText))
+    var keywords: string[] = []
+    if (!tags.includes(searchText))
     {
-      searchText = ''
+      keywords = searchText.split(' ').map(e => e.split('　')).flat()
     }
-    setRenderPosts(getPostsByTagsAndText(tags, searchText, tagToPostIds, idToPost, allPostIds))
+    setRenderPosts(getPostsByTagsAndText(tags, keywords, tagToPostIds, idToPost, allPostIds))
   }
   const onTagClick = (tag: string, isPushed: boolean) => {
     var nowSelectedTags: string[] = [];
@@ -98,6 +114,7 @@ export default function Home({
     setSearchText(text)
     updateRenderPosts(selectedTags, text)
   }
+  const sortKinds = ["新着", "閲覧数"]
 
   return (
     <Layout home>
@@ -105,10 +122,12 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <h1 className='my-4 font-bold text-2xl'>記事一覧</h1>
-      <h2 className='mt-4 mb-2 text-lg'>キーワードで絞り込む</h2>
+      <h2 className='mt-4 mb-2 text-lg'>キーワードで絞り込む (複数選んだ場合はAND検索)</h2>
       <Tags tags={tags} onClick={onTagClick}/>
-      <h2 className='mt-4 mb-2 text-lg'>任意の単語で絞り込む</h2>
+      <h2 className='mt-4 mb-2 text-lg'>任意の単語で絞り込む (スペース区切りでAND検索可)</h2>
       <Search onChange={onSearchChange}/>
+      <h2 className='mt-4 mb-2 text-lg'>表示順</h2>
+      <Dropdown items={sortKinds} />
       <section>
         <ul>
           <Posts postsData={renderPosts} path="/posts"/>
@@ -123,7 +142,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const tags = getAllTagList(allPostsData)
   var tagToPostIds = {} as TagToPostIds
   tags.forEach((tag) => {
-    tagToPostIds[tag] = searchPostIds(tag, allPostsData)
+    tagToPostIds[tag] = searchPostIds([tag], allPostsData)
   })
   var idToPost = {} as IdToPost
   allPostsData.forEach((post) => {
